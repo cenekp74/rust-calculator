c838df6d49bb4b634749e0fa97e81317e84c813d
Čeněk Použník <potuznikcenek@gmail.com>
2024-02-27 19:53:59 +0100

    parsing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,44 +7,45 @@ tauri::Builder::default()
     .expect("error while running tauri application");
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Token {
     Integer(i32),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Power,
+    OpenParent,
+    CloseParent,
+    End,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operator {
     Add,
     Sub,
-    Mul,
     Dev,
+    Mul,
     Pow,
-    Parenthesis(Vec<Token>)
+    Neg,
 }
 
-enum Function {
-    Add(Box<Node>, Box<Node>),
-    Sub(Box<Node>, Box<Node>),
-    Mul(Box<Node>, Box<Node>),
-    Dev(Box<Node>, Box<Node>),
-    Pow(Box<Node>, Box<Node>),
-}
-
-enum Node {
+#[derive(Debug)]
+enum Expression {
+    Binary(Operator, Box<Expression>, Box<Expression>),
+    Unary(Operator, Box<Expression>),
     Integer(i32),
-    Function(Function),
+    Arbitrary(Operator, Vec<Expression>),
 }
 
-fn tokenize(s: &str, parenthesis_level: usize) -> Result<(Vec<Token>, usize), String> {
+fn tokenize(s: &str) -> Result<Vec<Token>, String> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut last_c: char = ' ';
     let mut last_number: String = String::new();
-    let mut iterations_to_skip: usize = 0;
     for (i, c) in s.chars().enumerate() {
         if c == ' ' {
             continue;
         }
-        if iterations_to_skip > 0 {
-            iterations_to_skip -= 1;
-            continue; 
-        }
-        println!("{} ~~ {}", c, parenthesis_level);
         if c.is_digit(10) {
             last_number.push(c);
         } 
@@ -55,48 +56,173 @@ fn tokenize(s: &str, parenthesis_level: usize) -> Result<(Vec<Token>, usize), St
             }
         }
         if c == '+' {
-            tokens.push(Token::Add)
+            tokens.push(Token::Plus)
         }
         else if c == '-' {
-            tokens.push(Token::Sub)
+            tokens.push(Token::Minus)
         }
         else if c == '/' {
-            tokens.push(Token::Dev)
+            tokens.push(Token::Slash)
         }
         else if c == '*' {
-            if last_c == '*' {
-                tokens.pop();
-                tokens.push(Token::Pow)
-            }
-            else {
-                tokens.push(Token::Mul)
-            }
+            tokens.push(Token::Star)
         }
         else if c == '^' {
-            tokens.push(Token::Pow)
+            tokens.push(Token::Power)
         }
         else if c == '(' {
-            let (parenthesis_content, l) = tokenize(&s[i+1..], parenthesis_level+1).unwrap();
-            iterations_to_skip = l+1;
-            tokens.push(Token::Parenthesis(parenthesis_content));
+            tokens.push(Token::OpenParent)
         }
         else if c == ')' {
-            if !(parenthesis_level > 0) {
-                return Err(String::from("Parenthesis level error"))
-            }
-            return Ok((tokens, i))
+            tokens.push(Token::CloseParent)
         }
         last_c = c;
     }
     if !last_number.is_empty() {
         tokens.push(Token::Integer(last_number.parse::<i32>().unwrap()));
     }
-    Ok((tokens, 0))
+    tokens.push(Token::End);
+    Ok(tokens)
+}
+
+struct Parser {
+    current_index: usize,
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            current_index: 0,
+            tokens: tokens,
+        }
+    }
+    
+    fn consume_token(&mut self) {
+        self.current_index += 1;
+    }
+
+    fn current_token(&self) -> Result<Token, &str> {
+        if self.current_index >= self.tokens.len() {
+            return Err("Current index out of range");
+        }
+        Ok(self.tokens[self.current_index])
+    }
+    
+    fn primary(&mut self) -> Result<Expression, &str> {
+        let token = self.current_token().unwrap();
+        self.consume_token();
+        match token {
+            Token::Integer(n) => Ok(Expression::Integer(n)),
+            Token::OpenParent => {
+                let expr = self.expression().unwrap();
+                match self.current_token().unwrap() {
+                    Token::CloseParent => Ok(expr),
+                    _ => Err("syntaxerr"),
+                }
+            }
+            Token::Minus => {
+                let expr = self.factor().unwrap();
+                Ok(Expression::Unary(
+                    Operator::Neg,
+                    Box::new(expr),
+                ))
+            }
+            _ => {
+                Err("syntaxerr")
+            }
+        }
+    }
+
+    fn factor(&mut self) -> Result<Expression, &str> {
+        let expr = self.primary().unwrap();
+        let token = self.current_token().unwrap();
+        println!("{:?}", expr);
+        println!("{:?}", token);
+        match token {
+            Token::Power => {
+                self.consume_token();
+                let rhs = self.factor().unwrap();
+                Ok(Expression::Binary(
+                    Operator::Pow,
+                    Box::new(expr),
+                    Box::new(rhs),
+                ))
+            }
+            _ => {
+                Ok(expr)
+            }
+        }
+    }
+
+    fn term(&mut self) -> Result<Expression, &str> {
+        let mut expr = self.factor().unwrap();
+
+        loop {
+            let token = self.current_token().unwrap();
+            match token {
+                Token::Star => {
+                    self.consume_token();
+                    let rhs = self.factor().unwrap();
+                    expr = Expression::Binary(
+                        Operator::Mul,
+                        Box::new(expr),
+                        Box::new(rhs)
+                    );
+                }
+                Token::Slash => {
+                    self.consume_token();
+                    let rhs = self.factor().unwrap();
+                    expr = Expression::Binary(
+                        Operator::Dev,
+                        Box::new(expr),
+                        Box::new(rhs)
+                    );
+                }
+                _ => break,
+            };
+        }
+        Ok(expr)
+        
+    }
+
+    fn expression(&mut self) -> Result<Expression, &str> {
+        let mut expr = self.term().unwrap();
+        loop {
+            let token = self.current_token().unwrap();
+            match token {
+                Token::Plus => {
+                    self.consume_token();
+                    let rhs = self.term().unwrap();
+                    expr = Expression::Binary(
+                        Operator::Add,
+                        Box::new(expr),
+                        Box::new(rhs)
+                    );
+                }
+                Token::Minus => {
+                    self.consume_token();
+                    let rhs = self.term().unwrap();
+                    expr = Expression::Binary(
+                        Operator::Add,
+                        Box::new(expr),
+                        Box::new(rhs)
+                    );
+                }
+                _ => break,
+            };
+        }
+        Ok(expr)
+    }
 }
 
 #[tauri::command]
 fn process(input: &str) -> String {
-    let tokens: Vec<Token> = tokenize(input, 0).unwrap().0;
-    println!("{:?}", tokens);
-    return String::from("---");
+    let tokens: Vec<Token> = tokenize(input).unwrap();
+    // let s = format!("{:?}", tokens);
+    // return s;
+    let mut parser = Parser::new(tokens);
+    let res = parser.expression().unwrap();
+    let s = format !("{:?}", res);
+    s
 }
